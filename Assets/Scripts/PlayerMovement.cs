b8ee2b583800385c7a4ded92b80593f04ada15f7
liammckenzie <MCKELD3@student.op.ac.nz>
2024-08-01 14:13:59 +1200

    ADD: player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        transform.position += move * moveSpeed * Time.deltaTime;
+    }
+}
