2ad65a5e9c068f03ae16a7ce19d1bd6e4f5dbe3f
Liam McKenzie <mckeld3@student.op.ac.nz>
2024-08-02 15:45:27 +1200

    UPDATE: player controller to use character controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,83 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [System.Serializable]
+    public struct MovementSettings
+    {
+        public float maxSpeed;
+        public float acceleration;
+        public float deceleration;
+        public MovementSettings(float maxSpeed, float acceleration, float deceleration)
+        {
+            this.maxSpeed = maxSpeed;
+            this.acceleration = acceleration;
+            this.deceleration = deceleration;
+        }
+    }
+
+    [SerializeField] public MovementSettings groundSettings = new MovementSettings(10, 10, 10);
+    
+
     public float moveSpeed = 5f;
+    private Vector3 moveInput;
+    private Vector3 playerVelocity;
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
 
-        Vector3 move = transform.right * horizontal + transform.forward * vertical;
-        transform.position += move * moveSpeed * Time.deltaTime;
+        GroundMove();
+
+        characterController.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void GroundMove()
+    {
+        Vector3 wishDir = new Vector3(moveInput.x, 0, moveInput.z);
+        wishDir = transform.TransformDirection(wishDir);
+        wishDir.Normalize();
+        
+        float wishSpeed = wishDir.magnitude;
+        wishSpeed *= groundSettings.maxSpeed;
+
+        Accelerate(wishDir, wishSpeed, groundSettings.acceleration);
+
+
+
+    }
+
+    /// <summary>
+    /// calculates acceleration based on desired speed and direction.
+    /// </summary>
+    /// <param name="targetDir"></param>
+    /// <param name="targetSpeed"></param>
+    /// <param name="accel"></param>
+    private void Accelerate(Vector3 targetDir, float targetSpeed, float accel)
+    {
+        float currentSpeed = Vector3.Dot(playerVelocity, targetDir);
+        float addSpeed = targetSpeed - currentSpeed;
+        if (addSpeed <= 0)
+        {
+            return;
+        }
+
+        float accelSpeed = accel * Time.deltaTime * targetSpeed;
+        if (accelSpeed > addSpeed)
+        {
+            accelSpeed = addSpeed;
+        }
+        
+        playerVelocity.x += accelSpeed * targetDir.x;
+        playerVelocity.z += accelSpeed * targetDir.z;
     }
 }
