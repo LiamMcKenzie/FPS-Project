bbbca96a9398e4d574fcf5e64ef533945faf4f40
Liam McKenzie <mckeld3@student.op.ac.nz>
2024-08-02 17:23:06 +1200

    ADD: friction and jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This script is responsible for the player's movement
+/// </summary>
+/// <remarks>
+/// This script is based on the Quake 3 movement system translated by Isaiah Kelly.:
+/// https://github.com/IsaiahKelly/quake3-movement-for-unity
+/// </remarks>
 public class PlayerMovement : MonoBehaviour
 {
     [System.Serializable]
@@ -19,10 +26,13 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    [SerializeField] private float friction = 6;
+    [SerializeField] private float gravity = 20;
+    [SerializeField] private float jumpForce = 8;
+
     [SerializeField] public MovementSettings groundSettings = new MovementSettings(10, 10, 10);
     
 
-    public float moveSpeed = 5f;
     private Vector3 moveInput;
     private Vector3 playerVelocity;
     private CharacterController characterController;
@@ -36,13 +46,26 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
 
-        GroundMove();
+        if(characterController.isGrounded)
+        {
+            GroundMove();
+            if(Input.GetButtonDown("Jump"))
+            {
+                playerVelocity.y = jumpForce;
+            }
+        }
+        else
+        {
+            AirMove();
+        }
 
         characterController.Move(playerVelocity * Time.deltaTime);
     }
 
     void GroundMove()
     {
+        ApplyFriction(1f);
+
         Vector3 wishDir = new Vector3(moveInput.x, 0, moveInput.z);
         wishDir = transform.TransformDirection(wishDir);
         wishDir.Normalize();
@@ -51,9 +74,40 @@ public class PlayerMovement : MonoBehaviour
         wishSpeed *= groundSettings.maxSpeed;
 
         Accelerate(wishDir, wishSpeed, groundSettings.acceleration);
+    }
 
+    void AirMove()
+    {
+        playerVelocity.y -= gravity * Time.deltaTime;
+    }
 
+    private void ApplyFriction(float t)
+    {
+        Vector3 vec = playerVelocity;
+        vec.y = 0;
+        float speed = vec.magnitude;
+        float drop = 0;
 
+        if(characterController.isGrounded)
+        {
+            float control = (speed < groundSettings.deceleration) ? groundSettings.deceleration : speed;
+            drop = control * friction * Time.deltaTime * t;
+        }
+
+        float newSpeed = speed - drop;
+        friction = newSpeed;
+        if (newSpeed < 0)
+        {
+            newSpeed = 0;
+        }
+
+        if (speed > 0)
+        {
+            newSpeed /= speed;
+        }
+
+        playerVelocity.x *= newSpeed;
+        playerVelocity.z *= newSpeed;
     }
 
     /// <summary>
