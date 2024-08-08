fd20ad56c8df12198e934c5493a83eed380b60e3
Liam McKenzie <mckeld3@student.op.ac.nz>
2024-08-08 12:23:46 +1200

    ADD: random spawn position using box collider bounds

--- Assets/Scripts/Wave/SimpleEnemySpawner.cs
+++ Assets/Scripts/Wave/SimpleEnemySpawner.cs
@@ -10,12 +10,12 @@ public class SimpleEnemySpawner : MonoBehaviour
     {
         public int count;
         public GameObject enemyPrefab;
-        public Transform spawnPosition;
-        public EnemySquad(int count, GameObject enemyPrefab, Transform spawnPosition)
+        public BoxCollider spawnArea;
+        public EnemySquad(int count, GameObject enemyPrefab, BoxCollider spawnArea)
         {
             this.count = count;
             this.enemyPrefab = enemyPrefab;
-            this.spawnPosition = spawnPosition;
+            this.spawnArea = spawnArea;
         }
     }
 
@@ -23,17 +23,37 @@ public class SimpleEnemySpawner : MonoBehaviour
 
     void Start()
     {
-        for (int i = 0; i < enemies.Count; i++)
+        //spawns all enemies in the list
+        for (int i = 0; i < enemies.Count; i++) //loops through all "squads".
         {
-            for (int j = 0; j < enemies[i].count; j++)
+            for (int j = 0; j < enemies[i].count; j++) //loops through all enemies in the squad
             {
-                Spawn(enemies[i].enemyPrefab,enemies[i].spawnPosition.position);
-                
+                Spawn(enemies[i].enemyPrefab, GetRandomPosInBounds(enemies[i].spawnArea));
             }
         }
 
     }
 
+    /// <summary>
+    /// returns a random position within the bounds of a box collider. NOTE: always spawns at the bottom of the box collider
+    /// </summary>
+    /// <param name="boxCollider"></param>
+    /// <returns></returns>
+    public Vector3 GetRandomPosInBounds(BoxCollider boxCollider)
+    {
+        Bounds bounds = boxCollider.bounds;
+        return new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            bounds.min.y, //always spawn at bottom of box collider
+            Random.Range(bounds.min.z, bounds.max.z)
+        );
+    }
+
+    /// <summary>
+    /// spawns a prefab at a given position
+    /// </summary>
+    /// <param name="prefab"></param>
+    /// <param name="spawnPosition"></param>
     void Spawn(GameObject prefab, Vector3 spawnPosition)
     {
         Instantiate(prefab, spawnPosition, Quaternion.identity);
