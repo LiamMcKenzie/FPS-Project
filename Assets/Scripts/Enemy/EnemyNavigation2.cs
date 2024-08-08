b5854d78d69d31750d930fb96bfdbebf9aa99ce5
Liam McKenzie <mckeld3@student.op.ac.nz>
2024-08-08 16:23:37 +1200

    ADD: checkpoints to enemy navigation

--- Assets/Scripts/Enemy/EnemyNavigation2.cs
+++ Assets/Scripts/Enemy/EnemyNavigation2.cs
@@ -13,23 +13,43 @@ using UnityEngine.AI;
 
 public class EnemyNavigation2 : MonoBehaviour
 {
-    public GameObject[] targets;
+    public BoxCollider[] checkpoints;
+    private Vector3[] targets;
     public int targetIndex = 0;
     private NavMeshAgent agent;
 
     void Start()
     {
-        targets = GameObject.FindGameObjectsWithTag("Goal");
+        
+        //targets = GameObject.FindGameObjectsWithTag("Goal");
         agent = GetComponent<NavMeshAgent>();       
-        agent.SetDestination(targets[targetIndex].transform.position);     
+        agent.SetDestination(GetRandomPosInBounds(checkpoints[targetIndex]));
+        //agent.SetDestination(targets[targetIndex].transform.position);     
     }
 
     void Update()
     {
-        if (agent.remainingDistance < 0.5f && targetIndex < targets.Length - 1)
+        //if the agent has reached the target, then set the next destination
+        if (agent.remainingDistance < 0.5f && targetIndex < checkpoints.Length - 1)
         {
             targetIndex++;
-            agent.SetDestination(targets[targetIndex].transform.position);
+            agent.SetDestination(GetRandomPosInBounds(checkpoints[targetIndex]));
+            //agent.SetDestination(targets[targetIndex].transform.position);
         }
+    }
+
+    /// <summary>
+    /// returns a random position within the bounds of a box collider. NOTE: y axis is set to the bottom of the box
+    /// </summary>
+    /// <param name="boxCollider"></param>
+    /// <returns></returns>
+    public Vector3 GetRandomPosInBounds(BoxCollider boxCollider)
+    {
+        Bounds bounds = boxCollider.bounds;
+        return new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            bounds.min.y, //always choose bottom of box collider
+            Random.Range(bounds.min.z, bounds.max.z)
+        );
     }
 }
