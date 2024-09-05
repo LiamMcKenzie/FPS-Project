5178f6b9c6f17ae06a706a257dfae9620548fb16
Liam McKenzie <mckeld3@student.op.ac.nz>
2024-09-05 13:34:27 +1200

    ADD: bullet raycasting to refactored shoot script

--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -12,7 +12,8 @@ public enum WeaponType
 
 public class PlayerWeapon : MonoBehaviour
 {
-    public float fireRate = 0.5f; //NOTE: this needs to be refactored when I add other weapons, as each weapon will have its own fire rateq
+    public float fireRate = 0.5f; //NOTE: this needs to be refactored when I add other weapons, as each weapon will have its own fire rate
+    public float randomSpread = 0.1f; //NOTE: same as above, needs to be refactored
     
     private float shotCooldown; //used for fire rate
     private bool isShooting = false;
@@ -20,13 +21,13 @@ public class PlayerWeapon : MonoBehaviour
 
     public WeaponType currentWeapon = WeaponType.Pistol; //defaults to the pistol
 
-    // Start is called before the first frame update
+    [HideInInspector]public LayerMask layerMask; //layer mask for raycasting
+
     void Start()
     {
-        
+        layerMask = LayerMask.GetMask("Enemy", "World"); //gets the layer mask for the enemy layer and world layer.
     }
 
-    // Update is called once per frame
     void Update()
     {
         //TODO: assign upgradable values. (this could be a seperate function)
@@ -37,7 +38,7 @@ public class PlayerWeapon : MonoBehaviour
         {
             ShootInput();
             WeaponSwitchInput();
-            
+
             HandleShotCooldown(); //the shot cooldown only decreases when the player has control
         }
     }
@@ -91,14 +92,18 @@ public class PlayerWeapon : MonoBehaviour
 
 
     /// <summary>
-    /// Used when switching weapons so the shot cooldown is reset.
+    /// Used when switching weapons so the shot cooldown is reset. 
     /// </summary>
     void ResetShotCooldown()
     {
         shotCooldown = 0;
+        bufferedShot = false; //(shot buffer is reset, otherwise weapons would automatically shoot when switching)
     }
 
-    
+    /// <summary>
+    /// Adds to the shot cooldown, takes in a float for cooldown amount
+    /// </summary>
+    /// <param name="amount"></param>
     void AddShotCooldown(float amount) //Code to calculate the shot cooldown per weapon could also be calculated here
     {
         shotCooldown += amount;
@@ -149,6 +154,128 @@ public class PlayerWeapon : MonoBehaviour
         //TODO: play animation here
 
         //TODO: Switch case for different weapons and what function to shoot.
+        switch (currentWeapon)
+        {
+            case WeaponType.Pistol: 
+                ShootBullet();
+                break;
+
+            case WeaponType.Shotgun:
+                for (int i = 0; i < 5; i++) //shotgun shoots 5 bullets. TODO: use a variable for the number of bullets
+                {
+                    ShootBullet();
+                }
+
+                break;
+
+            case WeaponType.RocketLauncher:
+                //TODO: rocket launcher shoot function
+                break;
+
+            default:
+
+                break;
+        }
     }
 
+    //=====BULLET SHOOTING FUNCTIONS===== (shared between pistol and shotgun)
+
+    /// <summary>
+    /// Sorts an array of raycast hits by distance from the camera. Used for piercing bullets.
+    /// </summary>
+    /// <remarks>
+    /// This script was written using ChatGPT, since I've not used Array.Sort before.
+    /// </remarks>
+    /// <param name="hits"></param>
+    /// <returns></returns>
+    public RaycastHit[] SortRaycastHits(RaycastHit[] hits)
+    {
+        //Acording to this stackoverflow post, Array.sort uses an insertion sort for small arrays (our array is relatively small)
+        //https://stackoverflow.com/questions/1854604/which-sorting-algorithm-is-used-by-nets-array-sort-method
+
+        System.Array.Sort(hits, (hit1, hit2) =>
+        {
+            float distance1 = Vector3.Distance(Camera.main.transform.position, hit1.point);
+            float distance2 = Vector3.Distance(Camera.main.transform.position, hit2.point);
+            return distance1.CompareTo(distance2);
+        });
+
+        return hits;
+    }
+    
+    /// <summary>
+    /// Shoots a single raycast bullet and spawns a bullet trail.
+    /// TODO: refactor to check variables for either pistol or shotgun
+    /// </summary>
+    public void ShootBullet()
+    {
+        //assign ray position and direction
+        Vector3 rayOrigin = Camera.main,transform.position;
+        Vector3 rayDirection = Camera.main.transform.forward;
+
+        //adds shot randomization, needs to be refactored to fetch the appropriate weapon's spread value
+        rayDirection.x += Random.Range(-randomSpread, randomSpread);
+        rayDirection.y += Random.Range(-randomSpread, randomSpread);
+
+        //Sends out a raycastAll, which returns all objects hit by a raycast. The hits are unsorted by default.
+        //Raycast all is used for shot piercing
+        RaycastHit[] hits;
+        hits = SortRaycastHits(Physics.RaycastAll(rayOrigin, rayDirection, Mathf.Infinity, layerMask)); //sorts the hits by distance\
+
+        //Looping through all raycast hits, and either dealing damage or stopping loop
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+
+            if (hit.transform.tag == "Enemy") //if the raycast has hit an enemy, deal damage
+            {
+                hit.transform.GetComponent<EnemyHealth>().TakeDamage(damage);
+            }else //if the raycast didn't hit an enemy, it means it hit a wall. so break the loop
+            {
+                break;
+            }
+
+            if(bulletPiercing == false) //if player doesn't have bullet piercing stop the loop.
+            {
+                break;
+            }
+        }
+
+        //Spawn bullet trail
+        Vector3 hitPoint = rayOrigin + (rayDirection * 1000); //if nothing was hit, the bullet will travel 1000 units forward
+
+        if(hits.Length > 0) //if the raycast hit something, set the hitpoint to the furthest hit.
+        {
+            hitPoint = hits[hits.Length - 1].point; //if 5 objects were hit, hits.length will be 5. but the highest index would be 4. 
+        }
+
+        StartCoroutine(SpawnTrail(trail, hitPoint)); 
+    }
+
+    /// <summary>
+    /// Spawns a bullet trail that moves towards the hitpoint
+    /// </summary>
+    /// <param name="hitPoint"></param>
+    /// <returns></returns>
+    private IEnumerator SpawnTrail(Vector3 hitPoint)
+    {
+        TrailRenderer trail = Instantiate(trailRenderer, bulletSpawnPoint.position, Quaternion.identity);
+        Destroy(trail.gameObject, 1f); //destroys the trail after 1 second
+
+        Vector3 startPos = trail.transform.position;
+        float distance = Vector3.Distance(startPos, hitPoint);
+        float remaingDistance = distance;
+
+        float bulletSpeed = 300f;
+        
+
+        while (remaingDistance >= 0 && trail != null) //while bullet is stil moving towards target
+        {
+            trail.transform.position = Vector3.MoveTowards(trail.transform.position, hitPoint, bulletSpeed * Time.deltaTime);
+
+            remaingDistance = Vector3.Distance(trail.transform.position, hitPoint); //updates the distance to the target
+            
+            yield return null;
+        }
+    }
 }
